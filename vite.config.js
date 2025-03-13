763bba0ece563bf342299d7b2729c4219364aca2
G@nesh0606 <ganeshvaitkar20@gmail.com>
2025-03-13 20:14:27 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-review-sentiment-analyzer/',  // Repository name with correct case
+  base: '/movie-review-sentiment-analyzer/',  
   server: {
     proxy: {
       '/api': {
